67e8435f9a2fef8e75ebff6e2ed8efaf268da5de
Finn Dobbs <finndobbs@ku.edu>
2019-10-02 10:57:00 -0500

    Fix Bug

--- exercise1/mult.php
+++ exercise1/mult.php
@@ -1,9 +1,20 @@
-<?php
-
-function makeTable(){
-	for ($i=1; $i<=100; $i++){
-		for ($j=1; $j<=100; $j++){
-			$product = $i * $j;
-		}
-	}	
-}
+<!Doctype HTML>
+<html>
+<head>
+	<title>Multiplication Table</title>
+</head>
+<body>
+	<table>
+		<?php 		
+		for ($i=1; $i<=100; $i++){
+			echo '<tr>';
+			for ($j=1; $j<=100; $j++){
+				$product = $i * $j;
+				echo '<td>' .$i * $j. '</td>';
+			}
+			echo '</tr>';
+		}		
+		?>
+	</table>
+</body>
+</html>
