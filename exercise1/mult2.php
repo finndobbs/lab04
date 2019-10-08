c3bfee8629e6487a2f7ca39851741f119665e4d7
Finn Dobbs <finndobbs@ku.edu>
2019-10-08 16:36:08 -0500

    Fix column header spacing bug and clean code

--- exercise1/mult2.php
+++ exercise1/mult2.php
@@ -1,21 +1,22 @@
 <?php 	
-	echo '<style>table, th, td { border:1px solid black;}</style>';
+echo '<style>table, th, td { border:1px solid black;}</style>';
 	echo '<table>';
-	echo '<tr><td></td>';
-	for ($k=1; $k<=100; $k++){
-		echo '<th scope="col">' .$k. '<th>';
-	}
-	echo '</tr>';
-	for ($i=1; $i<=100; $i++){
-		#echo '<tr><th scope="col">' .$i. '</th></tr>';
-		echo '<tr>';	
-		echo '<th>' .$i. '</th>';
-		#echo '<th scope="col">' .$i. '</th>';
-		for ($j=1; $j<=100; $j++){
-			$product = $i * $j;
-			echo '<td>' .$i * $j. '</td>';
+	for ($i=0; $i<=100; $i++){
+		if ($i == 0){
+	            echo '<tr><td></td>';
+	            for ($k=1; $k<=100; $k++){
+	                echo '<th scope="col" class="col">' .$k. '</th>';
+	            }
+	            echo '</tr>';
+		}	
+		else{
+		    echo '<tr>';	
+		    echo '<th>' .$i. '</th>';
+		    for ($j=1; $j<=100; $j++){
+	                echo '<td>' .$i * $j. '</td>';
+		    }
+		    echo '</tr>';
 		}
-		echo '</tr>';
 	}	
 	echo '</table>';	
 ?>
