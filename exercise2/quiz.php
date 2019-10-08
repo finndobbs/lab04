1a1166d203d8f47d7254f00cd66530da685f31fa
Finn Dobbs <finndobbs@ku.edu>
2019-10-08 17:37:48 -0500

    Fix assigning value of answers, print all answers

--- exercise2/quiz.php
+++ exercise2/quiz.php
@@ -1,5 +1,14 @@
 <?php
 
-$q1 = $_POST["q1"]['value'];
-echo 'you answered ' .$q1. '!'; 
+$q1 = $_POST["q1"];
+$q2 = $_POST["q2"];
+$q3 = $_POST["q3"];
+$q4 = $_POST["q4"];
+$q5 = $_POST["q5"];
+
+echo 'you answered ' .$q1. '!<br>'; 
+echo 'you answered ' .$q2. '!<br>'; 
+echo 'you answered ' .$q3. '!<br>'; 
+echo 'you answered ' .$q4. '!<br>'; 
+echo 'you answered ' .$q5. '!<br>'; 
 ?>
